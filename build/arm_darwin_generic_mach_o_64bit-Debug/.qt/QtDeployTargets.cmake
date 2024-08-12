86e66e1eab2d8202b8ed944657dae2e98dc30933
y0ub <y0lit7515@gmail.com>
2024-08-12 11:37:45 +0900

    delete: build directory from managiment with git

--- build/arm_darwin_generic_mach_o_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_y0_todo_FILE /Users/y0_/prog/cc/y0_todo/build/arm_darwin_generic_mach_o_64bit-Debug/y0_todo.app/Contents/MacOS/y0_todo)
--- build/arm_darwin_generic_mach_o_64bit-Debug/CMakeFiles/y0_todo.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for y0_todo.
-# This may be replaced when dependencies are built.
