9062092f543acd988411d0071db46720ad0c5b80
y0ub <y0lit7515@gmail.com>
2024-08-12 12:09:08 +0900

    add: infinit table model.

---
+++ widgets/infinitetablemodel.cpp
@@ -0,0 +1,55 @@
+#include "infinitetablemodel.h"
+
+InfiniteTableModel::InfiniteTableModel(QObject *parent) : QAbstractTableModel(parent) {}
+
+int InfiniteTableModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return row_count_value; // 大きな行数を設定
+}
+
+int InfiniteTableModel::columnCount(const QModelIndex &parent ) const
+{
+    Q_UNUSED(parent);
+    return column_count_value; // 大きな列数を設定
+}
+
+QVariant InfiniteTableModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid()) {
+        return QVariant();
+    }
+
+    if (role == Qt::DisplayRole) {
+        // 行と列のインデックスに基づいてデータを生成
+        return QString("TODO Item (%1, %2)").arg(index.row()).arg(index.column());
+    }
+    return QVariant();
+}
+
+QVariant InfiniteTableModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role != Qt::DisplayRole) {
+        return QVariant();
+    }
+
+    if (orientation == Qt::Horizontal) {
+        // 横のヘッダー（例: 日付）
+        return QString("Date %1").arg(section);
+    } else {
+        // 縦のヘッダー（例: 時刻）
+        return QString("Time %1").arg(section);
+    }
+}
+
+void InfiniteTableModel::SetRowCount(int count)
+{
+    row_count_value = count;
+    emit layoutChanged();
+}
+
+void InfiniteTableModel::SetColumnCount(int count)
+{
+    column_count_value = count;
+    emit layoutChanged();
+}
---
+++ widgets/infinitetablemodel.h
@@ -0,0 +1,33 @@
+#ifndef INFINITETABLEMODEL_H
+#define INFINITETABLEMODEL_H
+
+#include <QAbstractTableModel>
+#include <QString>
+#include <QDate>
+#include <QTime>
+
+class InfiniteTableModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+public:
+    InfiniteTableModel(QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+    void SetRowCount(int count);
+
+    void SetColumnCount(int count);
+
+private:
+    int row_count_value = 1000; // 大きな行数
+    int column_count_value = 1000; // 大きな列数
+};
+
+#endif // INFINITETABLEMODEL_H
