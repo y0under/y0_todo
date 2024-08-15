3aa2cae3871d7dce8586139a3d8df84353f21bb2
y0ub <y0lit7515@gmail.com>
2024-08-15 09:25:35 +0900

    update: use spin box for to select hour and minute.

--- dialog/todoitemediter.cpp
+++ dialog/todoitemediter.cpp
@@ -12,6 +12,11 @@ TodoItemEditer::TodoItemEditer(QWidget *parent)
 
     ui->task_date->setDate(QDate::currentDate());
 
+    ui->hour_spinBox->setRange(0, 23);
+
+    ui->minute_spinBox->setSingleStep(5);
+    ui->minute_spinBox->setRange(0, 55);
+
     connect(ui->select_date_button, &QPushButton::clicked, this, &TodoItemEditer::showCalendarDialog);
 }
 
